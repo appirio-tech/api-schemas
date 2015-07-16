3ee3d24b31e09907449354a87675faa4052ecfe5
Andrew Selbie <aselbie@gmail.com>
2015-07-16 16:40:29 -0700

    Add Apiary sync

---
+++ resources.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { url: 'https://api.apiary.io/blueprint/get/messaging14',
+    savePath: __dirname + '/apiary/messaging14.json' },
+  { url: 'https://api.apiary.io/blueprint/get/apworkmicroservice',
+    savePath: __dirname + '/apiary/apworkmicroservice.json' }
+];
